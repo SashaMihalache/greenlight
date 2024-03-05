390d5e7144d5d5bcb1c9f6d6dc92e773babe643f
Sasha Mihalache <sasha.mihalache@gmail.com>
2024-03-05 09:25:35 +0200

    pass a mock movie with json enconding from struct;

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"greenlight.sashamihalache.com/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,5 +20,20 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "Show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered an error", http.StatusInternalServerError)
+	}
+
 }
