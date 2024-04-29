1606c2dd634b9ffb26c2597b317f3a75827333dc
Sasha Mihalache <sasha.mihalache@gmail.com>
2024-04-29 21:52:31 +0300

    refactor errors

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -14,7 +14,6 @@ func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Reques
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
